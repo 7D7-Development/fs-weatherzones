34fc54d310be53f36ce402a794b8184214f24f6c
7D7-Development <93505100+7D7-Development@users.noreply.github.com>
2024-11-20 12:02:28 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
-
-author 'GhostFaceScripts'
-description 'GhostFace Weatherzones'
+lua54 'yes'
+author 'GhostFaceScripts/7D7-Development'
+description 'GhostFace Weatherzones - Debug Config EDIT'
 version '1.0.0'
-
 client_scripts {
 	'config.lua',
 	'client/client.lua',
 }
-
-lua54 'yes'
